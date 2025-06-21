083a8530a56c886ce53e73e2448e8e67c99b15c3
vkoryakin768 <gudvin596@gmail.com>
2025-06-21 20:57:41 +0300

    Update Main.java

--- src/main/java/com/gmail/gudvin596/Main.java
+++ src/main/java/com/gmail/gudvin596/Main.java
@@ -3,10 +3,11 @@ package com.gmail.gudvin596;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static String msg = "Hello qa.quru!!!";
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello qa.quru!!!");
+        System.out.printin(msg);
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
